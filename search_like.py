05a241715d2edaf815b49f5d8ba1d6e8733932bb
collyerm <62969333+collyerm@users.noreply.github.com>
2020-04-18 18:46:07 +1000

    Update search_like.py

--- search_like.py
+++ search_like.py
@@ -3,7 +3,7 @@ import sqlite3
 def query_search_like(search_string):
     connie = sqlite3.connect('sw.db')
     c = connie.cursor()
-    search_term = search_string + '%'
+    search_term = '%' + search_string + '%'
     c.execute("SELECT charID, name FROM characters WHERE name LIKE ?", (search_term,))
     list = c.fetchall()
     return (list)
